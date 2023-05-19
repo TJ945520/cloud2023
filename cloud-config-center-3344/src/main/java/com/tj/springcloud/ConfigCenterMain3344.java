d631d468f4abefece277a1c12c8e5839ddfab919
tanjie <tanjie@tj.com>
2023-05-19 15:31:30 +0800

    springcloud-config配置文件-hot-fix-1

---
+++ cloud-config-center-3344/src/main/java/com/tj/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.tj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author TANJIE
+ * @date 2023/5/19 15:27
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
